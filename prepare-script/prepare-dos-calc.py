c2a7d8ee5033f5fda500d5f3be96a9a7c6697c75
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2017-10-03 21:17:25 +0200

    fix prepare script simple topology

--- prepare-script/prepare-dos-calc.py
+++ prepare-script/prepare-dos-calc.py
@@ -3,21 +3,19 @@
 import gromacstools as gt
 
 # modify this stuff to your needs!
-atomtypes = [{'name': 'C',  'mass': 12.011},
-             {'name': 'H',  'mass':  1.008},
-             {'name': 'CL', 'mass': 35.453},
-             {'name': 'CH', 'mass': 13.019}]
+atomtypes = [{'name': 'O',  'mass': 15.9994},
+             {'name': 'H',  'mass':  1.008}]
 
-simpletop = [{'name': "CHC", 'atomtypes': [3, 2, 2, 2], 'nmols': 512,
-              'sigma': 3, 'abc_indicators': [2, 3, 1, -1]}]
+simpletop = [{'name': "SOL", 'atoms': [atomtypes[j] for j in [0, 1, 1]], 'nmols': 500,
+              'sigma': 2, 'abc_indicators': [1, 2, 0, -1]}]
 
 nsamples = 1
-nblocks = 5
-nblocksteps = 5000
+nblocks = 1
+nblocksteps = 30
 outputfile = "params.txt"
 # stop modifying here
 
 top = gt.top.Topology()
-top.load_simple_top(simpletop, atomtypes)
+top.load_simple_top(simpletop)
 top.save_2pt_parameters_file(nsamples, nblocks, nblocksteps,
                              parameters_file=outputfile)
