459fa694ca62b3718999ed50154e97bf28156ae8
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2018-01-29 17:33:52 +0100

    changed prepare script

--- prepare-script/prepare-dos-calc.py
+++ prepare-script/prepare-dos-calc.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 
-import gromacstools as gt
-
-# modify this stuff to your needs!
+# modify these lines to your needs!
 atomtypes = [{'name': 'O',  'mass': 15.9994},
              {'name': 'H',  'mass':  1.008}]
 
-simpletop = [{'name': "SOL", 'atoms': [atomtypes[j] for j in [0, 1, 1]], 'nmols': 5,
-              'sigma': 2, 'abc_indicators': [1, 2, 0, -1], 'rot_treat': 'f'},
-             {'name': "H2O2", 'atoms': [atomtypes[j] for j in [1, 0, 0, 1]], 'nmols': 2,
-              'sigma': 2, 'abc_indicators': [0, 3, 1, -1], 'rot_treat': 'f'}]
+moltypes = [{'name': "SOL", 'atoms': [atomtypes[j] for j in [0, 1, 1]], 'nmols': 500,
+             'sigma': 2, 'abc_indicators': [1, 2, 0, -1], 'rot_treat': 'f'},
+            {'name': "H2O2", 'atoms': [atomtypes[j] for j in [1, 0, 0, 1]], 'nmols': 2,
+             'sigma': 2, 'abc_indicators': [0, 3, 1, -1], 'rot_treat': 'f'}]
 
 nsamples = 2
 nblocks = 2
@@ -17,7 +15,18 @@ nblocksteps = 1000
 outputfile = "params.txt"
 # stop modifying here
 
-top = gt.top.Topology()
-top.load_simple_top(simpletop)
-top.save_2pt_parameters_file(nsamples, nblocks, nblocksteps,
-                             parameters_file=outputfile)
+with open(outputfile, 'w') as f:
+
+    f.write(str(nsamples) + "\n")
+    f.write(str(nblocks) + "\n")
+    f.write(str(nblocksteps) + "\n")
+    f.write(str(len(moltypes)) + "\n")
+    for moltype in moltypes:
+        moltype_atommasses = (atom['mass'] for atom in moltype['atoms'])
+        moltype_string = f"{moltype['nmols']} "\
+                         f"{len(moltype['atoms'])} "\
+                         f"{' '.join(map(str, moltype_atommasses))} "\
+                         f"{moltype['rot_treat']} "\
+                         f"{' '.join(map(str, moltype['abc_indicators']))}"
+
+        f.write(moltype_string + '\n')
