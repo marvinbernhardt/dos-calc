a4699af64b10587464a28313816735643bc75a6a
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2019-06-25 15:42:27 +0200

    show error margins by default

--- scripts/show-dos.py
+++ scripts/show-dos.py
@@ -17,6 +17,8 @@ parser.add_argument('-c', '--components', help='show components DoS',
                     dest='show_components', action='store_true')
 parser.add_argument('-x', '--cross', help='show cross DoS',
                     dest='show_cross', action='store_true')
+parser.add_argument('-s', '--samples', help='show samples individually',
+                    dest='show_samples', action='store_true')
 
 args = parser.parse_args()
 
@@ -46,13 +48,12 @@ doses = [
     {'name': 'dos_x_rot_vib', 'tags': ['cross']},
 ]
 
-dos_trn_files = glob.glob('sample*_dos_trn.txt')
+samples = [int(dos_trn_file.split('_')[0].split('e')[1]) for dos_trn_file in glob.glob('sample*_dos_trn.txt')]
 
-if len(dos_trn_files) == 0:
+if len(samples) == 0:
     raise Exception("ERROR: No dos found to show!")
-else:
-    for dos_trn_file in dos_trn_files:
-        sample = int(dos_trn_file.split('_')[0].split('e')[1])
+elif len(samples) == 1 or args.show_samples:
+    for sample in samples:
 
         # filter which doses to show
         doses_to_show = []
@@ -96,4 +97,52 @@ else:
             plt.xlabel("freqency in 1/ps")
         fig.legend()
 
+    plt.show()
+
+else:
+    # filter which doses to show
+    doses_to_show = []
+    for dos in doses:
+        if 'standard' in dos['tags']:
+            doses_to_show.append(dos)
+        if 'comp' in dos['tags'] and args.show_components:
+            doses_to_show.append(dos)
+        if 'cross' in dos['tags'] and args.show_cross:
+            doses_to_show.append(dos)
+
+    # load data from files
+    for dos in doses_to_show:
+
+        dos['data'] = []
+        for sample in samples:
+            dos['data'].append(np.array(pd.read_csv(f"sample{sample}_{dos['name']}.txt", sep=' ', header=None)))
+
+        dos['mean'] = np.array(dos['data']).mean(axis=0)
+        dos['std'] = np.array(dos['data']).std(axis=0)
+
+    # show dos plots
+    fig, ax = plt.subplots()
+    for moltype in range(len(doses_to_show[0]['data'][0])):
+        for dos in doses_to_show:
+            if frequencies:
+                line, = ax.plot(frequencies, dos['mean'][moltype], label=f"{moltype} {dos['name']}")
+                ax.fill_between(frequencies,
+                                dos['mean'][moltype]-dos['std'][moltype],
+                                dos['mean'][moltype]+dos['std'][moltype],
+                                facecolor=line.get_color(), alpha=0.3)
+            else:
+                arbitrary_freqs = np.arange(0, len(dos['mean'][moltype]))
+                line, = ax.plot(dos['mean'][moltype], label=f"{moltype} {dos['name']}")
+                ax.fill_between(arbitrary_freqs,
+                                dos['mean'][moltype]-dos['std'][moltype],
+                                dos['mean'][moltype]+dos['std'][moltype],
+                                facecolor=line.get_color(), alpha=0.3)
+
+    if frequencies:
+        plt.xlabel("freqency in 1/ps")
+        plt.ylabel("DoS in ps")
+    else:
+        plt.xlabel("freqency in arbitrary units")
+        plt.ylabel("DoS in arbitrary units")
+    fig.legend()
     plt.show()
